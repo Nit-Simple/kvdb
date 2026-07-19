18fabb50553b190d65e8e5de306f9a27e9d63c4b
nithul <nithulshamej2@gmail.com>
2026-07-19 21:16:05 +0530

    made the file write and read to capture queries through tcp connection instead of stdin

--- src/kv_store.rs
+++ src/kv_store.rs
@@ -3,8 +3,10 @@ use std::{
     collections::HashMap,
     fs::{File, OpenOptions},
     io::{BufRead, BufReader, BufWriter, Write},
+    net::TcpStream,
     path::PathBuf,
     process,
+    sync::{Arc, Mutex},
 };
 
 use crate::command::Command;
@@ -13,6 +15,68 @@ pub struct KvStore {
     map: HashMap<String, String>,
     path: PathBuf,
     writer: BufWriter<File>,
+}
+
+pub fn handle_client(stream: TcpStream, locked_store: Arc<Mutex<KvStore>>) -> anyhow::Result<()> {
+    let mut reader = BufReader::new(stream);
+
+    loop {
+        let mut line = String::new();
+
+        match reader.read_line(&mut line) {
+            std::result::Result::Ok(0) => {
+                println!("client disconnected");
+                break;
+            }
+
+            std::result::Result::Ok(_) => match Command::read_from_line(&line.trim()) {
+                Err(e) => {
+                    let response_stream = reader.get_mut();
+                    writeln!(response_stream, "{}", e)?;
+                    response_stream.flush()?;
+                    continue;
+                }
+
+                std::result::Result::Ok(Command::Set { key, value }) => {
+                    let response_stream = reader.get_mut();
+                    let mut locked_gaurd = locked_store.lock().unwrap();
+                    locked_gaurd.set(key, value)?;
+                    writeln!(response_stream, "OK")?;
+                    response_stream.flush()?;
+                }
+                std::result::Result::Ok(Command::Get { key }) => {
+                    let response_stream = reader.get_mut();
+                    let locked_gaurd = locked_store.lock().unwrap();
+                    match locked_gaurd.get(&key) {
+                        Some(value) => writeln!(response_stream, "value : {}", value)?,
+                        None => writeln!(response_stream, "nil")?,
+                    }
+                    response_stream.flush()?;
+                }
+                std::result::Result::Ok(Command::Delete { key }) => {
+                    let response_stream = reader.get_mut();
+                    let mut locked_gaurd = locked_store.lock().unwrap();
+                    locked_gaurd.delete(key)?;
+                    writeln!(response_stream, "Ok")?;
+                    response_stream.flush()?;
+                }
+                std::result::Result::Ok(Command::Exit) => {
+                    let response_stream = reader.get_mut();
+                    writeln!(response_stream, "Goodbye")?;
+                    response_stream.flush()?;
+                    break;
+                }
+            },
+
+            Err(e) => {
+                let response_stream = reader.get_mut();
+                writeln!(response_stream, "Error {}", e)?;
+                response_stream.flush()?;
+                break;
+            }
+        }
+    }
+    Ok(())
 }
 
 impl KvStore {
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,31 @@
 mod command;
 mod kv_store;
 
-use std::io::{self, BufRead, Write};
+use std::{
+    net::TcpListener,
+    sync::{Arc, Mutex},
+    thread,
+};
 
-use command::Command;
 use kv_store::KvStore;
 
 fn main() -> anyhow::Result<()> {
-    let stdin = io::stdin();
-    let mut reader = io::BufReader::new(stdin.lock());
-    let mut kvstore = KvStore::new("log/store.log".into())?;
-    loop {
-        print!("=> ");
-        io::stdout().flush()?;
-        let mut line = String::new();
-        if reader.read_line(&mut line)? == 0 {
-            break;
-        }
-
-        match Command::read_from_line(&line) {
-            Err(e) => {
-                print!("{}", e);
-                continue;
+    let kvstore = KvStore::new("log/store.log".into())?;
+    let store = Arc::new(Mutex::new(kvstore));
+    let listener = TcpListener::bind("127.0.0.1:8082")?;
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                let store_clone = Arc::clone(&store);
+                thread::spawn(move || {
+                    if let Err(e) = kv_store::handle_client(stream, store_clone) {
+                        eprintln!("client error {}", e);
+                    }
+                });
             }
-            Ok(Command::Get { key }) => match kvstore.get(&key) {
-                None => println!("=> nil"),
-                Some(value) => println!("=> {}", value),
-            },
-            Ok(Command::Set { key, value }) => kvstore.set(key, value)?,
-            Ok(Command::Delete { key }) => kvstore.delete(key)?,
-            Ok(Command::Exit) => kvstore.exit(),
+            Err(e) => {
+                eprintln!("connection failed {}", e);
+            }
         }
     }
     Ok(())
